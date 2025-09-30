fe51f94f8d833569b8503ee00405b15c511222fe
fel1x210 <luunguyenminhtriet021025@gmail.com>
2025-09-29 22:59:45 -0400

    Initial commit for UI Prototype (Capstone Project's UI) 😘� 👌 👌 👌 👌 😘�

---
+++ app/src/main/java/ca/gbc/comp3074/uiprototype/MainActivity.java
@@ -0,0 +1,78 @@
+package ca.gbc.comp3074.uiprototype;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView bottomNavigationView;
+    private final Fragment homeFragment = new HomeFragment();
+    private final Fragment searchFragment = new SearchFragment();
+    private final Fragment favoritesFragment = new FavoritesFragment();
+    private final Fragment profileFragment = new ProfileFragment();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        // Initialize bottom navigation
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        // Set up bottom navigation listener
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            int itemId = item.getItemId();
+            if (itemId == R.id.navigation_home) {
+                switchFragment(homeFragment);
+                return true;
+            } else if (itemId == R.id.navigation_search) {
+                switchFragment(searchFragment);
+                return true;
+            } else if (itemId == R.id.navigation_favorites) {
+                switchFragment(favoritesFragment);
+                return true;
+            } else if (itemId == R.id.navigation_profile) {
+                switchFragment(profileFragment);
+                return true;
+            }
+            return false;
+        });
+
+        // Load default fragment
+        if (savedInstanceState == null) {
+            bottomNavigationView.setSelectedItemId(R.id.navigation_home);
+            switchFragment(homeFragment);
+        }
+    }
+
+    private void switchFragment(Fragment fragment) {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.fragment_container, fragment)
+                .commit();
+    }
+
+    public void navigateToProfile() {
+        bottomNavigationView.setSelectedItemId(R.id.navigation_profile);
+        switchFragment(profileFragment);
+    }
+
+    public void navigateToSearch() {
+        bottomNavigationView.setSelectedItemId(R.id.navigation_search);
+        switchFragment(searchFragment);
+    }
+}
